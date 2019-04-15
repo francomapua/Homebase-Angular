861d0cf117005f432195b34693129d65ec2fa5f5
Franco Luis Mapua <fmapua@gmail.com>
2019-04-15 22:29:37 +0800

    Update services.component.ts

--- playground/src/app/services/services.component.ts
+++ playground/src/app/services/services.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from "@angular/core";
-import { DataService } from "../data.service";
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
-  selector: "app-services",
-  templateUrl: "./services.component.html",
-  styleUrls: ["./services.component.scss"]
+  selector: 'app-services',
+  templateUrl: './services.component.html',
+  styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent implements OnInit {
-  constructor(private data : DataService) {
+
+  users : Object;
+
+  constructor(private data: DataService) {
     // Use data to reference
   }
 
-  ngOnInit() {}
+  // Angular Lifecycle hook that runs when the component is started
+  ngOnInit() {
+
+    // Call the Data Service's getUsers() function
+    console.log('Getting users');
+    this.data.getUsers().subscribe((data) => {
+      console.log('users received');
+      this.users = data;
+      console.log(data);
+    });
+  }
 
   serviceCall() {
     this.data.secondClick();
   }
+
+  // HTTP Client
+
 }
